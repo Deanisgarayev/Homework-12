639b4e4caf292f109f2e134a3520ace9a8f0a3f6
Денис <garaevdenis2019@gmail.com>
2023-03-21 00:30:15 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author();
-        author.firstName = "Alexander";
-        author.lastName = "Pushkin";
-        System.out.println("author.firstName = " + author.firstName);
-        System.out.println("author.lastName = " + author.lastName);
-        Book book = new Book();
-        book.name = "Ruslan and Lyudmila";
-        book.author = "Alexander Sergeyevich Pushkin";
-        book.dateOfPublication = 1820;
-        System.out.println("book.name = " + book.name);
-        System.out.println("book.author = " + book.author);
-        System.out.println("book.dateOfPublication = " + book.dateOfPublication);
+        Author firstAuthor = new Author("Alexander", "Pushkin");
+        System.out.println("firstAuthor = " + firstAuthor.firstName);
+        System.out.println("firstAuthor = " + firstAuthor.lastName);
+
+        Author seconAuthor = new Author("Leo", "Tolstoy");
+        System.out.println("secondAuthor.firstName = " + seconAuthor.firstName);
+        System.out.println("secondAuthor.lastName = " + seconAuthor.lastName);
+
+        Book firstBook = new Book("Ruslan and Lyudmila", "Alexander Sergeyevich Pushkin", 1820);
+        System.out.println("firstBook = " + firstBook.name);
+        System.out.println("firstBook = " + firstBook.author);
+        System.out.println("firstBook = " + firstBook.dateOfPublication);
+
+        Book secondBook = new Book("War and peace", "Leo Nikolayevich Tolstoy", 1865);
+        System.out.println("secondBook.name = " + secondBook.name);
+        System.out.println("secondBook.author = " + secondBook.author);
+        System.out.println("secondBook.dateOfPublication = " + secondBook.dateOfPublication);
+        secondBook.setDateOfPublication(1869);
+        System.out.println("secondBook.dateOfPublication = " + secondBook.dateOfPublication);
     }
 }
-    //String name;
-    //int age;
-//}
-
-//public class Main {
-//    public static void main(String[] args) {
-//        Person sarah = new Person();
-//        sarah.name = "Sarah";
-//        System.out.println("sarah.name = " + sarah.name);
-//        sarah.age = 30;
-//        System.out.println("sarah.age = " + sarah.age);
-//    }
-//} Alexander Sergeyevich Pushkin
